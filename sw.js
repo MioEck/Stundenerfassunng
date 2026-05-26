7fc6d4bea2dd2f38b5a1105bd30105e145089381
MioEck <macbook33@googlemail.com>
2026-05-26 22:22:08 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker — macht die App offline-fähig und installierbar
-const CACHE_NAME = 'stundenzettel-v19';
+const CACHE_NAME = 'stundenzettel-v20';
 const URLS = [
   './stundenzettel.html',
   './manifest.json',
